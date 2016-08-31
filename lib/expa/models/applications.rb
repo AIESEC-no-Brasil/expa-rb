d8484c3474b88bb7820101ddf9f9c81abdf21d0a
luancorumba <luan@corumba.net>
2016-08-31 03:40:15 -0300

    Corrigindo o analytics

--- lib/expa/models/applications.rb
+++ lib/expa/models/applications.rb
@@ -192,7 +192,7 @@ module EXPA::Applications
     end
 
     def url_return_analytics
-      url_return_all_applications + 'analyse'
+      url_return_all_applications + 'analyze.json'
     end
 
     def url_view_application_attributes(id)
