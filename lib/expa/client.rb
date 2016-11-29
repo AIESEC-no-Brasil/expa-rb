8a74e8c960706aa6871f00bf9779a234562b4a52
luancorumba <luan@corumba.net>
2016-11-29 15:57:05 -0200

    add login by OP

--- lib/expa/client.rb
+++ lib/expa/client.rb
@@ -3,6 +3,7 @@ module EXPA
 
     def initialize
       @url = 'https://auth.aiesec.org/users/sign_in'
+      @url_op = 'https://opportunities.aiesec.org/auth'
       @token = nil
       @max_age = nil
       @expiration_time = nil
@@ -62,6 +63,42 @@ module EXPA
           index = cj.count
           for i in 0...index
             index = i if cj.to_a[i].domain == 'experience.aiesec.org'
+            puts i
+          end
+          if index != cj.count
+            token = cj.to_a[index].value
+            expiration_time = cj.to_a[index].created_at
+            max_age = cj.to_a[index].max_age
+            puts 'token: '+token.to_s
+            puts 'expiration_time: '+expiration_time.to_s
+            puts 'max_age: '+max_age.to_s
+          end
+        end
+      end
+      {token:token,expiration_time:expiration_time,max_age:max_age}
+    end
+
+    def login_opportunities(email, password)
+      agent = Mechanize.new {|a| a.ssl_version, a.verify_mode = 'TLSv1',OpenSSL::SSL::VERIFY_NONE}
+      page = agent.get(@url_op)
+      aiesec_form = page.forms[0]
+      aiesec_form.field_with(:name => 'user[email]').value = email
+      aiesec_form.field_with(:name => 'user[password]').value = password
+
+      begin
+        page = agent.submit(aiesec_form, aiesec_form.buttons.first)
+        puts aiesec_form.buttons.first
+        puts page.content
+        puts page.code.to_i
+      rescue => exception
+        puts exception.to_s
+        false
+      else
+        if page.code.to_i == 200
+          cj = page.mech.agent.cookie_jar.store
+          index = cj.count
+          for i in 0...index
+            index = i if cj.to_a[i].domain == 'opportunities.aiesec.org '
             puts i
           end
           if index != cj.count
