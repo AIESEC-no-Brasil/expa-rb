cf41dd05f42ad82cebd10a000d97d37f04c4d25a
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-03-19 23:35:03 -0300

    Committees#get

--- lib/expa/models/offices.rb
+++ lib/expa/models/offices.rb
@@ -15,6 +15,40 @@ end
 
 module EXPA::Offices
   class << self
+    def list_single_office(xp_id)
+      single_office_list_json({}, xp_id)
+    end
+  end
 
+
+  private
+
+  def single_office_list_json(params = {}, xp_id)
+    params['access_token'] = EXPA.client.get_updated_token
+    params['committee_id'] = xp_id
+
+    uri = URI(url_return_committee(xp_id))
+    uri.query = URI.encode_www_form(params)
+
+    force_get_response(uri)
+  end
+
+  def url_return_committee(xp_id)
+    $url_api + "committees/#{xp_id}"
+  end
+
+  def force_get_response(uri)
+    i = 0
+    while i < 50
+      begin
+        res = Net::HTTP.get(uri)
+        res = JSON.parse(res) unless res.nil?
+        i = 50
+      rescue => exception
+        puts exception.to_s
+        sleep(i)
+      end
+    end
+    res
   end
 end
