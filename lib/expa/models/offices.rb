bf2a2839cd8d1746df87922c768645594c720bc9
luancorumba <luan@corumba.net>
2017-04-11 13:06:35 -0300

    office parent id

--- lib/expa/models/offices.rb
+++ lib/expa/models/offices.rb
@@ -1,5 +1,6 @@
 class Office
   attr_accessor :id
+  attr_accessor :parent_id
   attr_accessor :name
   attr_accessor :full_name
   attr_accessor :url
@@ -7,6 +8,7 @@ class Office
 
   def initialize(json)
     self.id = json['id'].to_i unless json['id'].nil?
+    self.parent_id = json['parent_id'].to_i unless json['id'].nil?
     self.name = json['name'] unless json['name'].nil?
     self.full_name = json['full_name'] unless json['full_name'].nil?
     self.url = URI(json['url']) unless json['url'].nil?
